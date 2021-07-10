d40e910af77a3deaa73a509326c35224651d3d4c
crigigal <50228889+crigigal@users.noreply.github.com>
2021-07-10 16:15:52 +0100

    Updates Updates

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+const img = "https://picsum.photos/100";
+
+const customStyle = {
+  color: "blue",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+
+customStyle.color = "pink";
 
 ReactDOM.render(
   <div>
-    <h1>My Websites</h1>
-    <ul>
-      <li>Sapsapiens</li>
-      <li>Trasteros</li>
-      <li>Cristina Gallo</li>
-      <li>GoGuiri</li>
-    </ul>
+    <h1 className="heading">My Fav Pictures</h1>
+    <h2 style={customStyle}>Angela's course</h2>
+    <div>
+      <img src={img + "?grayscale"} alt="" />
+      <img src={img} alt="" />
+      <img src={img + "?grayscale"} alt="" />
+    </div>
   </div>,
   document.getElementById("root")
 );
