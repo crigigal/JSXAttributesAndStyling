ecf79624eab53f529a769ad66801a4ce02e81c7e
crigigal <50228889+crigigal@users.noreply.github.com>
2021-07-10 15:10:10 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Websites</h1>
+    <ul>
+      <li>Sapsapiens</li>
+      <li>Trasteros</li>
+      <li>Cristina Gallo</li>
+      <li>GoGuiri</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
